1cdb211d7b9be8072b3aa1340ee9ab1b52989325
gebrina <gsonofmika@gmail.com>
2023-09-02 20:06:13 +0300

    implemented quiz-category module

--- src/quiz-category/quiz-category.service.ts
+++ src/quiz-category/quiz-category.service.ts
@@ -1,4 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { QuizCategory } from 'src/entities/quiz-category.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class QuizCategoryService {}
+export class QuizCategoryService {
+  constructor(
+    @InjectRepository(QuizCategory)
+    private readonly quizCategoryRepo: Repository<QuizCategory>,
+  ) {}
+
+  async findAll(): Promise<QuizCategory[]> {
+    return await this.quizCategoryRepo.find();
+  }
+
+  async findOne(id: string): Promise<QuizCategory> {
+    return await this.quizCategoryRepo.findOneBy({ id });
+  }
+
+  async create(quizCategory: QuizCategory): Promise<QuizCategory> {
+    return await this.quizCategoryRepo.save(quizCategory);
+  }
+
+  async update(quizCategory: QuizCategory): Promise<QuizCategory> {
+    let categorytTbeUpdated = await this.findOne(quizCategory.id);
+    if (!categorytTbeUpdated)
+      throw new NotFoundException('Invalid category id');
+    categorytTbeUpdated = quizCategory;
+    return this.quizCategoryRepo.save(categorytTbeUpdated);
+  }
+
+  async delete(id: string): Promise<string> {
+    await this.quizCategoryRepo.delete(id);
+    return id;
+  }
+}
